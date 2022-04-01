99b9c278343fb096881c0d079a7fafb5bec61928
gethin <gethin.yan@gmail.com>
2022-04-01 17:06:54 +0800

    JZOffer13

--- JZOffer/JZ13/main.go
+++ JZOffer/JZ13/main.go
@@ -7,10 +7,41 @@ import (
 )
 
 func main() {
-	m, n, k := 2, 3, 1
+	m, n, k := 16, 8, 4
 	fmt.Println(movingCount(m, n, k))
 }
 
 func movingCount(m int, n int, k int) int {
-	return 0
+	flag := make([][]bool, m)
+	for i := 0; i < m; i++ {
+		flag[i] = make([]bool, n)
+	}
+
+	flag[0][0] = true
+
+	count := 1
+	for i := 0; i < m; i++ {
+		sumI := sumDigit(i)
+		for j := 0; j < n; j++ {
+			if (i > 0 && flag[i-1][j]) || (j > 0 && flag[i][j-1]) {
+				sumJ := sumDigit(j)
+				if sumI+sumJ <= k {
+					flag[i][j] = true
+					count++
+				}
+			}
+		}
+	}
+
+	return count
+}
+
+func sumDigit(number int) int {
+	sum := 0
+	for number >= 1 {
+		sum += number % 10
+		number /= 10
+	}
+
+	return sum
 }
